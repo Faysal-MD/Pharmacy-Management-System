3648a4fe12dd3a4d1e8b7470f1aebb2cd58e474d
Faysal598 <faysalmahmud735@gmail.com>
2022-12-02 19:05:29 -0800

    final commit

---
+++ PharmacyManagementsystem/PharmacyManagementsystem/src/Project/Connectionprovider.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project;
+import java.sql.*;
+
+/**
+ *
+ * @author Akib
+ */
+public class Connectionprovider {
+    public static Connection getcon (){
+           {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/pharmacy","root","");
+            return con;
+        } catch (Exception e) {
+           return null;
+        }
+    }
+    }
+}
+ 
+    
